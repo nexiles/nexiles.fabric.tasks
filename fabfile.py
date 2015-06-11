793ea2bca8a9c49b1343be43a16f8eb52834c4bb
Stefan Eletzhofer <se@nexiles.de>
2015-06-11 10:50:42 +0200

    Build System
    Fix fabfile and config.
    
    Signed-off-by: Stefan Eletzhofer <se@nexiles.de>

--- fabfile.py
+++ fabfile.py
@@ -1,9 +1,14 @@
 import os
+import logging
 
 from fabric.api import env
 from fabric.api import task
+from fabric.api import hide
 from fabric.api import local
 from fabric.api import execute
+
+from nexiles.fabric.tasks import log
+log.setup_logging(level=logging.ERROR, logfile=None)
 
 from nexiles.fabric.tasks import docs
 from nexiles.fabric.tasks import utils
@@ -11,31 +16,26 @@ from nexiles.fabric.tasks import release
 from nexiles.fabric.tasks import environment
 
 PACKAGE_NAME = "nexiles.fabric.tasks"
-VERSION      = utils.get_version_from_setup_py()
-
-ROOT_DIR     = os.path.abspath(os.path.dirname(__file__))
-BUILD_DIR    = "{ROOT_DIR}/dist".format(**globals())
-
-DIST_DIR     = "~/develop/nexiles/dist/{PACKAGE_NAME}/{PACKAGE_NAME}-{VERSION}".format(**globals())
-
-SRC_PACKAGE  = "{BUILD_DIR}/{PACKAGE_NAME}-{VERSION}.tar.gz".format(**globals())
-DOC_PACKAGE  = "{BUILD_DIR}/{PACKAGE_NAME}-doc-{VERSION}.tar.gz".format(**globals())
 PUBLIC_DIR   = "/Volumes/skynet-wt-10-2/ptc/Windchill_10.2/HTTPServer/htdocs/docs/{PACKAGE_NAME}".format(**globals())
 
-env.nexiles.update(
-    package_name=PACKAGE_NAME,
-    version=VERSION,
-    root_dir=ROOT_DIR,
-    doc_package=DOC_PACKAGE,
+env.nexiles.initialize(
+    package_name  = PACKAGE_NAME,
     doc_public_dir=PUBLIC_DIR
 )
+
+
+@task
+def version():
+    """Print package version"""
+    log.info("{} {}".format(env.nexiles.package_name, env.nexiles.version))
 
 
 @task
 def build():
     execute(docs.build)
     execute(docs.package)
-    local("python setup.py clean sdist develop")
+    with hide("running", "stdout"):
+        local("python setup.py clean sdist develop")
 
 
 @task
